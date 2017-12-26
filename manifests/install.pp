36defbb2285938e8417855ebd8d32291c95baeb4
Randika Hewage <randikaudara@gmail.com>
2017-12-26 22:39:11 +0530

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,6 @@
 class ntp::install (
-  $package_name   = $::ntp::package_name,
-  $package_ensure = $::ntp::package_ensure,
+  String $package_name   = $::ntp::package_name,
+  String $package_ensure = $::ntp::package_ensure,
 ){
 package { $package_name:
   ensure => $package_ensure,
