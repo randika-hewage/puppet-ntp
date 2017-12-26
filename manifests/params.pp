7f84ae4a05a03870aa3d8c9d3c3500bb82b85683
Randika Hewage <randikaudara@gmail.com>
2017-12-26 23:28:49 +0530

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,15 +3,21 @@ class ntp::params {
   $package_ensure = 'present'
   $config_name = 'ntp.conf'
   $config_file_mode = '0664'
-  $servers = ['0.centos.pool.ntp.org', '1.centos.pool.ntp.org']
+  
+  case $facts['os']['family'] {
+    'Debian': { $server_list = ['0.centos.pool.ntp.org', '1.centos.pool.ntp.org'] }
+    'RedHat': { $server_list = ['0.centos.pool.ntp.org', '1.centos.pool.ntp.org'] }
+    default: { fail("This ${facts['os']['name']}  not supported by this module") }
+  }
+  #$server_list = ['0.centos.pool.ntp.org', '1.centos.pool.ntp.org']
   $service_ensure = 'running'
   $service_enable = true
   $service_hasretart = true
   $service_hasrestart = true
   $service_hasstatus = true
   
-  $servce_name = $facts['os']['family'] ? {
+  $service_name = $facts['os']['family'] ? {
   'Redhat' => 'ntpd',
   'Debian' => 'ntp',
-  default => fail("This ${facts['os']['name']}  not supported by this module")
+  default => fail("This ${facts['os']['name']}  not supported by this module"),
 }
