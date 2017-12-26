afb8818b5e05638fee035467bc2ef593bbec3f78
Randika Hewage <randikaudara@gmail.com>
2017-12-26 23:37:45 +0530

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,7 +42,22 @@
 #
 # Copyright 2017 Your name here, unless otherwise noted.
 #
-class ntp {
+class ntp (
+  String $package_name        = $::ntp::params::package_name,
+  String $package_ensure      = $::ntp::params::package_ensure,
+  String $config_name         = $::ntp::params::config_name,
+  String $config_file_mode    = $::ntp::params::config_file_mode,
+  Array[String] $server_list  = $::ntp::params::server_list,
+  String $service_ensure      = $::ntp::params::service_ensure,
+  String $service_name        = $::ntp::params::service_name,
+  Boolean $service_enable     = $::ntp::params::service_enable,
+  Boolean $service_hasrestart = $::ntp::params::service_hasrestart,
+  Boolean $service_hasstatus  = $::ntp::params::service_hasstatus,
+) inherits ::ntp::params {
 
+class { '::ntp::install': }
+class { '::ntp::config': }
+class { '::ntp::service': }
 
+Class['::ntp::install'] -> Class['::ntp::config'] ~> Class['::ntp::service']
 }
